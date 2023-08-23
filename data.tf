f299ab6acf6868e5ec2123f3bc0d095c9578eab3
mstiesto <mstiesto01@gmail.com>
2023-08-23 17:38:30 +0200

    update tf files

--- data.tf
+++ data.tf
@@ -1,4 +1,4 @@
 data "aws_route53_zone" "zones" {
-  for_each = {for value in local.hosted_zones: value => value }  
+  for_each = {for zone_name in var.zone_names: zone_name => zone_name }  
   name     = each.value
 }
