783bb02ddf4fd5d345ac7baaf2557c4f07b15b27
mstiesto <mstiesto01@gmail.com>
2023-08-23 17:52:30 +0200

    update tf files

--- main.tf
+++ main.tf
@@ -1,21 +1,19 @@
 resource "aws_route53_record" "mx_records" {
-  for_each = {for zone_name in var.zone_names: zone_name => zone_name }
+  for_each = { for zone_name in var.zone_names: zone_name => zone_name }
   
-  zone_id = data.aws_route53_zone.zones[each.key].zone_id
-  name    = each.value
+  name    = ""
   type    = "MX"
   ttl     = 120
-
+  zone_id = data.aws_route53_zone.zones[each.key].zone_id
   records = var.mx_records[each.key]
 }
 
 resource "aws_route53_record" "txt_records" {
-  for_each = {for zone_name in var.zone_names: zone_name => zone_name }
+  for_each = { for zone_name in var.zone_names: zone_name => zone_name }
 
-  zone_id = data.aws_route53_zone.zones[each.key].zone_id
-  name    = each.value
+  name    = ""
   type    = "TXT"
   ttl     = 120
-
+  zone_id = data.aws_route53_zone.zones[each.key].zone_id
   records = var.txt_records[each.key]
 }
