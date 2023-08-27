01945d3cf9c1daadfc63eec1d1306db58e38c58c
mstiesto <mstiesto01@gmail.com>
2023-08-27 13:46:40 +0200

    Add providers

--- main.tf
+++ main.tf
@@ -1,26 +1,26 @@
 resource "aws_route53_record" "cname_records" {
   for_each = var.records
     zone_id = data.aws_route53_zone.all_zones[each.key].zone_id
-    name    = var.records[each.key].cname.name
+    name    = var.records[each.key].cname.url
     type    = "CNAME"
     ttl     = 300
-    records = [var.records[each.key].cname.value]
+    records = [var.records[each.key].cname.record]
 }
 
 resource "aws_route53_record" "mx_records" {
   for_each = var.records
     zone_id = data.aws_route53_zone.all_zones[each.key].zone_id
-    name    = var.records[each.key].mx.name
+    name    = var.records[each.key].mx.url
     type    = "MX"
     ttl     = 300
-    records = ["0 ${var.records[each.key].mx.value}"]
+    records = ["0 ${var.records[each.key].mx.record}"]
 }
 
 resource "aws_route53_record" "txt_records" {
   for_each = var.records
     zone_id = data.aws_route53_zone.all_zones[each.key].zone_id
-    name    = var.records[each.key].txt.name
+    name    = var.records[each.key].txt.url
     type    = "TXT"
     ttl     = 300
-    records = [var.records[each.key].txt.value]
+    records = [var.records[each.key].txt.record]
 }
